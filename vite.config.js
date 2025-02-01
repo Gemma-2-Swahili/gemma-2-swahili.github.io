bd078f6cdd0f2b26cc444130a30f5dce6634944b
Alfaxad Eyembe <68440833+Alfaxad@users.noreply.github.com>
2025-02-02 04:44:39 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/gemma2-swahili/', // This is required for GitHub Pages project sites
+  base: '/', // This is required for GitHub Pages project sites
   plugins: [react()]
 })
